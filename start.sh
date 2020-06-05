28dfdb10b6b46ed19a32d32c04f6d4289be93e96
dagger <daggergad7@gmail.com>
2020-06-05 20:38:41 +0530

    final fixes #2

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
-	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
-	echo "on-download-stop=./delete.sh" >> aria2c.conf
+	echo -e "$RCLONE_CONFIG" > rclone.conf
 	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
-	chmod +x delete.sh
+	echo "on-download-stop=./on-stop.sh" >> aria2c.conf
 	chmod +x on-complete.sh
+	chmod +x on-stop.sh
 fi
 
 echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
-aria2c --conf-path=aria2c.conf&
+aria2c -q --conf-path=aria2c.conf&
 yarn start

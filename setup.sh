49f9fbd6bc0f6d6758caae39c9b701a0f3c00d5f
dag <daggergad7@gmail.com>
2020-11-02 12:27:53 +0530

    remove aria install from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
-# Install aria2c static binary
-wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
+# # Install aria2c static binary
+# wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+# tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+# export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
 
 # Create download folder
 mkdir -p downloads
